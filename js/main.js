3dcd4cb453d9f7a27af3e9efc1749a4b52abd326
unknown <pje224@naver.com>
2025-08-14 15:24:24 +0900

    비주얼/상품 스와이퍼 작성 완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 window.addEventListener("load" , function(){
-    
+    const swiper = new Swiper(".product-imgs", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+        const swiper2 = new Swiper(".outdoor-imgs", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
 })
